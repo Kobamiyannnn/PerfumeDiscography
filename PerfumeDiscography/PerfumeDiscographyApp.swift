d35ae1ea8823b8820e10614b553aa8ff2651a524
Kobamiyannnn <kobaprfm23@gmail.com>
2023-02-22 15:54:56 +0900

    Initial Commit

---
+++ PerfumeDiscography/PerfumeDiscographyApp.swift
@@ -0,0 +1,18 @@
+//
+//  PerfumeDiscographyApp.swift
+//  PerfumeDiscography
+//  
+//  Created by Kobamiyannnn on 2023/02/22
+//  
+//
+
+import SwiftUI
+
+@main
+struct PerfumeDiscographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
